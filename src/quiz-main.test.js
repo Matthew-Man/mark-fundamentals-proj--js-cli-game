d828d6913399968bf45850f7dd28a729d2eb7fe2
Matthew Man <matthew08m@gmail.com>
2021-02-03 14:57:51 +0000

    updates to test and package files

--- src/quiz-main.test.js
+++ src/quiz-main.test.js
@@ -1,1 +1,5 @@
-const {} = require("./quiz-main");
+const { createCategories } = require("./quiz-main");
+
+test("createCategories returns an object with categories and answers", () => {
+    expect(createCategories()).toBe('"CONTINENT": ["EUROPE", "ASIA", "AFRICA", "NORTH AMERICA", "SOUTH AMERICA", "ANTARTICA", "OCEANIA"], "COLOUR": ["RED", "BLUE", "GREEN", "PINK", "PURPLE"], "SPORT": ["TENNIS", "BASKETBALL", "FOOTBALL", "VOLLEYBALL", "HOCKEY"], "UK CITY": ["LONDON", "BIRMINGHAM", "MANCHESTER", "LIVERPOOL", "YORK"], "CODING LANGUAGE": ["PYTHON", "JAVASCRIPT", "JAVA", "TYPESCRIPT", "RUBY", "C"]');
+});
