136561be54f6c7d66b261d4909e82b98113160db
Matthew Man <matthew08m@gmail.com>
2021-02-03 14:46:13 +0000

    introduction text function and variables added

--- src/quiz-main.js
+++ src/quiz-main.js
@@ -2,11 +2,31 @@
 const prompt = require("readline-sync").question;
 
 
-// Main quiz game
+const seperator = "\n--------------------------------------\n";
+
+
+function printIntroduction() {
+    console.log("\nWelcome to the Academy Anagram Game! 👋 ");
+    console.log(`\nHOW TO COMPLETE THE GAME:
+    - Choose a category.
+    - Unscramble the jumbled word and type out your guess.
+    - If you get it wrong, not to worry, you can get a clue!
+    - Guess all the words from each category!
+    - However, be careful not to get too many wrong!
+    - NOTE: YOU ONLY HAVE 3 LIVES!👀
+    `);
+};
+
+
+// Main quiz game --------------------------
+
+
 function playQuiz() {
 
 };
 
+
+// Exports --------------------------
 
 module.exports = {
     playQuiz,
